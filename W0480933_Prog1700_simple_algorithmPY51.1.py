3fa5f937a338182383dd0c0fac8a18fcfeb03e28
aidenmackinnon <146340697+aidenmackinnon@users.noreply.github.com>
2023-09-28 08:19:43 -0300

    Add files via upload

---
+++ W0480933_Prog1700_simple_algorithmPY51.1.py
@@ -0,0 +1,39 @@
+
+#Author: Davis Boudreau
+
+#Student ID: <W0480933>
+
+#Course: PROG1700
+
+#Date: 09/26/23
+
+#Project: Create a simple Algorithm using the program Flowchart and pseudocode
+
+#Repository: <GitHub URL>
+
+#Programming Language: Python 3
+
+#License: Creative Commons
+
+#Version: 1.0
+
+def myfunc():
+    global x
+age=int(input("how old are you?:")) #user defines the age value
+x="age"                        
+def myfunc():                
+    global currentyear
+
+currentyear=2023       #defining the value of current year variable
+def myfunc():
+    global z
+z=currentyear-age       #the z value is the calculated total of subtracting those two values
+z #z is the year of birth variable
+print("year of birth:") 
+print(z) if age>(0) else print("output error") # z value is year of birtth, if the age is less than one it prints an output error
+
+
+
+
+
+
